84ddfeae36a6c2c2e948e908b22dab0425b521f5
kaspernj <k@spernj.org>
2021-12-31 19:17:57 +0000

    Added dependabot, more gem stuff, Rubocop offences etc

--- Gemfile
+++ Gemfile
@@ -6,9 +6,11 @@ source "https://rubygems.org"
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
 group :development do
-  gem "rspec"
   gem "bundler"
+  gem "rspec"
 
-  gem "best_practice_project", "0.0.12", require: false
+  gem "best_practice_project", require: false
   gem "rubocop", "1.24.1", require: false
+  gem "rubocop-performance", require: false
+  gem "rubocop-rspec", require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require "simple_delegate"
-
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
@@ -8,7 +6,4 @@ require "simple_delegate"
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-RSpec.configure do |config|
-end
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
