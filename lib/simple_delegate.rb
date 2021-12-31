c88ae59e1a74e329b7d7c39fd766e8b1eb43a817
kaspernj <k@spernj.org>
2021-12-31 19:36:27 +0000

    Ruby 3 support for args and opts

--- lib/simple_delegate.rb
+++ lib/simple_delegate.rb
@@ -4,11 +4,9 @@ module SimpleDelegate
   end
 
   module ClassMethods
-    def delegate(*args)
-      opts = args.pop
-
+    def delegate(*args, **opts)
       args.each do |method|
-        define_method(method) do |*method_args, &method_blk|
+        define_method(method) do |*method_args, **method_opts, &method_blk|
           if opts[:to_instance_variable]
             object = instance_variable_get("@#{opts.fetch(:to_instance_variable)}")
             raise "Nothing was returned by: #{opts.fetch(:to_instance_variable)}" unless object
@@ -19,7 +17,7 @@ module SimpleDelegate
             raise "Don't know where to delegate to"
           end
 
-          object.__send__(method, *method_args, &method_blk)
+          object.__send__(method, *method_args, **method_opts, &method_blk)
         end
       end
     end
--- spec/simple_delegate_spec.rb
+++ spec/simple_delegate_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 class SampleClass1
   include SimpleDelegate
 
-  delegate :method1, :method2, to_instance_variable: :object
+  delegate :method1, :method2, :method3, to_instance_variable: :object
 
   def initialize
     @object = SampleClass2.new
@@ -17,6 +17,10 @@ class SampleClass2
 
   def method2
     "method2"
+  end
+
+  def method3(arg1, first_name:)
+    "method3 #{arg1} first name: #{first_name}"
   end
 end
 
@@ -41,5 +45,10 @@ describe "SimpleDelegate" do
     sample = DelegateToMethods.new
     expect(sample.method1).to eq "method1"
     expect(sample.method2).to eq "method2"
+  end
+
+  it "delegates arguments and options" do
+    sample = SampleClass1.new
+    expect(sample.method3("test", first_name: "Kasper")).to eq "method3 test first name: Kasper"
   end
 end
